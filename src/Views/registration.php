e9e5c1d9aec6839021a0c4c710d255bf0256d16b
mysit <89347844+mysit@users.noreply.github.com>
2026-05-25 23:06:02 +0300

    Update registration.php

--- src/Views/registration.php
+++ src/Views/registration.php
@@ -13,8 +13,9 @@ unset($_SESSION['form_errors'], $_SESSION['old_data']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="/8/public/">
     <title>Коты — форма заявки</title>
-    <link rel="stylesheet" href="/8/public/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Unbounded:wght@200..900&display=swap" rel="stylesheet">
